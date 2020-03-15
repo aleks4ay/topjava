8e27b506a06bf48a2c5c8ebbb6e20c99680798e8
Aleksey Sergienko <aleks4ay@gmail.com>
2020-03-15 09:43:02 +0200

    hw5 other

--- src/test/java/ru/javawebinar/topjava/service/ServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/ServiceTest.java
@@ -29,6 +29,6 @@ import static ru.javawebinar.topjava.UserTestData.*;
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 @ActiveProfiles(resolver = ActiveDbProfileResolver.class)
-public class ServiceTest {
+public abstract class ServiceTest {
 
 }
